db712fd1f65a14039bbdc7d84286371b9f64c2a2
davih22997 <davih22997@gmail.com>
2025-01-26 13:44:17 +0100

    Cargando hecho

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpriteAnimator : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer; // Asigna el SpriteRenderer desde el Inspector
+    public Image sourceImage; // Asigna el SpriteRenderer desde el Inspector
     public Sprite[] sprites; // Lista de imágenes a mostrar
-    public float frameRate = 0.1f; // Tiempo entre frames
+    private float frameRate = 0.2f; // Tiempo entre frames
 
     private int currentFrame = 0;
 
     void Start()
     {
-        if (spriteRenderer == null)
-            spriteRenderer = GetComponent<SpriteRenderer>();
+        if (sourceImage == null)
+            return;
+        sourceImage.sprite = sprites[currentFrame];
 
         if (sprites.Length > 0)
             StartCoroutine(PlayAnimation());
@@ -22,9 +24,10 @@ public class SpriteAnimator : MonoBehaviour
     {
         while (true)
         {
-            spriteRenderer.sprite = sprites[currentFrame];
-            currentFrame = (currentFrame + 1) % sprites.Length;
+            Debug.Log("Frame " + currentFrame);
             yield return new WaitForSeconds(frameRate);
+            currentFrame = (currentFrame + 1) % sprites.Length;
+            sourceImage.sprite = sprites[currentFrame];
         }
     }
 }
